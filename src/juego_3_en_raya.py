ed94eed28fc9fec7bb87f6ed1d02d1665a236766
saranieves92 <saranieves.matheu@um.es>
2025-02-10 16:02:55 +0100

    añadida comprobación de jugada ganadora

--- src/juego_3_en_raya.py
+++ src/juego_3_en_raya.py
@@ -70,3 +70,25 @@ def test_movimiento_incorrecto():
     x= 2
     y= 3
     assert False == movimiento_valido(x,y,movimientos_otro_jugador)
+
+def jugada_ganadora(movimientos_jugador):
+    """
+    Método que permite determinar si los movimientos de un jugador le permite ganar una partida.
+
+    Parámetros:
+        * movimientos_jugador: dict con el conjunto de movimientos de un jugador
+    """
+    #Comprobamos si hay 3 fichas en una fila
+    for fila in movimientos_jugador:
+        movimientos_columna = movimientos_jugador[fila]
+        if len(movimientos_columna)==3:
+            return True
+    return False
+
+def test_no_ganador():
+    movimientos_jugador={2:[2,3]}
+    assert False == jugada_ganadora(movimientos_jugador)
+
+def test_ganador():
+    movimientos_jugador={2:[1,2,3]}
+    assert True == jugada_ganadora(movimientos_jugador)
